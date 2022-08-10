a1a3884615cb10bedabf4786cb9e95756c1a394e
zirg3 <zirg31@gamil.com>
2022-08-10 23:57:56 +0300

    v3

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -9,23 +9,23 @@ module.exports = function(app) {
       })
   );
   app.use(
-      '/api',
-      createProxyMiddleware({
-        target: 'https://sport-app-test.herokuapp.com/',
-        changeOrigin: true,
-      })
-  );
-  app.use(
-      '/uploads',
-      createProxyMiddleware({
-        target: 'https://sport-app-test.herokuapp.com/',
-        changeOrigin: true,
-      })
-  );
-  app.use(
       '/uploads',
       createProxyMiddleware({
         target: 'http://localhost:5000',
+        changeOrigin: true,
+      })
+  );
+  app.use(
+      '/api',
+      createProxyMiddleware({
+        target: 'mongodb+srv://sport-app:Oleg3456@cluster0.66rgyqd.mongodb.net/test',
+        changeOrigin: true,
+      })
+  );
+  app.use(
+      '/uploads',
+      createProxyMiddleware({
+        target: 'mongodb+srv://sport-app:Oleg3456@cluster0.66rgyqd.mongodb.net/test',
         changeOrigin: true,
       })
   );
