c585ac19ecdff0cef416f08d7d01cab952a7bcc9
zirg3 <zirg31@gamil.com>
2022-08-11 12:01:58 +0300

    fix6

--- back/server.js
+++ back/server.js
@@ -36,10 +36,10 @@ app.use('/api/workouts', workoutRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   // Step 1:
-  app.use(express.static(path.resolve(__dirname, "./front/build")));
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
   // Step 2:
   app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./front/build", "index.html"));
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 }
 
